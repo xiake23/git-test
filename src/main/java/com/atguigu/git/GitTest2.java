b0da5bc3a2bb4eaecf1b122011bcf52a7630d2cc
Zheng <1182733571@qq.com>
2021-09-25 16:43:38 +0800

    3.1 commit

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -9,5 +9,6 @@ package com.atguigu.git;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("TEST success");
+        System.out.println("修改2");
     }
 }
