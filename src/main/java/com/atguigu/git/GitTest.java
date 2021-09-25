e9808d9f6a6d113357c67dac626d0479d6a84b68
xiake23 <88986512+xiake23@users.noreply.github.com>
2021-09-25 20:52:57 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
         System.out.println("push test");
-
+        System.out.println("pull test");
     }
 }
